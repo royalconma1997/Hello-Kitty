7525af65bc62d9108e96cb8779239b0ee8906f2b
hanavuu <vuukimxuan@gmail.com>
2018-04-19 08:12:39 +0700

    gggg

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello kitty");
+            Console.WriteLine("anhoxeo");
         }
     }
 }
